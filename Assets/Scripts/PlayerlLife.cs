07c05caad13e3428d43c0d735887e3f2d764fb01
mmitchell25-dos <mmitchell25@elam.co.uk>
2025-11-21 11:21:19 +0000

    WE'RE NEARLY THERE

--- Assets/Scripts/PlayerlLife.cs
+++ Assets/Scripts/PlayerlLife.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 public class PlayerlLife : MonoBehaviour
 {
+    bool dead = false;
+
+    private void Update()
+    {
+        if (transform.position.y < -1f && !dead)
+        {
+            Die();
+        }
+
+    }
+
+
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Enemy Body"))
         {
+            GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<Rigidbody>().isKinematic = true;
+            GetComponent<PlayerMovement>().enabled = false;
             Die();
         }
 
@@ -16,11 +31,9 @@ public class PlayerlLife : MonoBehaviour
 
     void Die()
     {
-        GetComponent<MeshRenderer>().enabled = false;
-        GetComponent<Rigidbody>().isKinematic = true;
-        GetComponent<PlayerMovement>().enabled = false;
         Reloadlevel();
         Invoke(nameof(Reloadlevel), 1.3f);
+        
     }
 
 
@@ -29,6 +42,7 @@ public class PlayerlLife : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+
 
 
 
